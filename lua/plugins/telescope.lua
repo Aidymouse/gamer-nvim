9a74d36fbb492c07b761e897fabf7122635a13c5
Aidan Habedank <aidan@thewizardscat.co.nz>
2025-03-24 23:04:58 +1300

    [Telescope] add more keys

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,5 +7,6 @@ return {
 	      { "<leader>sf", ":Telescope find_files<return>", desc="[S]earch [F]iles"},
 	      { "<leader>sg", ":Telescope live_grep<return>", desc="Live [G]rep"},
 	      { "<leader>sr", ":Telescope resume<return>", desc="[R]esume Search"},
+	      { "<leader><leader>", ":Telescope buffers<return>", desc="Telescope Open Buffers"},
 	}
 }
