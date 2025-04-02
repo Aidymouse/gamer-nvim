0b34541948912e197c6ee9425c9784a410dd1cfb
Aidan <aidan@thewizardscat.co.nz>
2025-04-02 21:19:37 +1300

    [Keys] go to different windows with ctrl+direction

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -7,12 +7,12 @@ return {
     -- refer to the configuration section below
   },
   keys = {
-    {
-      "<leader>?",
-      function()
-        require("which-key").show({ global = false })
-      end,
-      desc = "Buffer Local Keymaps (which-key)",
-    },
+    { "<leader>?", function() require("which-key").show({ global = false }) end, desc = "Buffer Local Keymaps (which-key)", },
+    { "<C-l>", "<C-w>l", desc = "Move to right window", },
+    { "<C-k>", "<C-w>k", mode="", desc = "Move to above window", },
+    { "<C-h>", "<C-w>h", mode="", desc = "Move to left window", },
+    { "<C-j>", "<C-w>j", mode="", desc = "Move to below window", },
+
+    -- Random config keys I put here just cos
   },
 }
